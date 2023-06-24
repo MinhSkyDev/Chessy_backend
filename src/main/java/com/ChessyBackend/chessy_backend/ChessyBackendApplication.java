a3d899661fb223f5e067a2f4df5d4744abe65c51
MinhSkyDev <minhdev2002@gmail.com>
2023-06-24 22:28:52 +0700

    fixing syntax error and fixing Firebase Config

--- src/main/java/com/ChessyBackend/chessy_backend/ChessyBackendApplication.java
+++ src/main/java/com/ChessyBackend/chessy_backend/ChessyBackendApplication.java
@@ -7,22 +7,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Objects;
 
 @SpringBootApplication
 public class ChessyBackendApplication {
-	public static void main(String[] args) throws IOException {
-			ClassLoader classLoader = ChessyBackendApplication.class.getClassLoader();
-			File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
 
-			FileInputStream serviceAccount =
-					new FileInputStream(file.get);
+
+
+	public static void main(String[] args) throws IOException {
+//			ClassLoader classLoader = ChessyBackendApplication.class.getClassLoader();
+//			File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
+
+
+		InputStream serviceAccount =
+				new ClassPathResource("serviceAccountKey.json").getInputStream();
+
+//			FileInputStream serviceAccount =
+//					new FileInputStream(FirebaseService.getFile().getAbsolutePath());
 			FirebaseOptions options = new FirebaseOptions.Builder()
 					.setCredentials(GoogleCredentials.fromStream(serviceAccount))
 					.build();
